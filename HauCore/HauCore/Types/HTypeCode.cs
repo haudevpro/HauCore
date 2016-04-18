3505ae58cd78473eac43f6b8d3e09c77368c684e
Nguyễn Văn Hậu <nguonsang@hotmail.com.vn>
2016-04-18 09:31:47 +0700

    test

---
+++ HauCore/HauCore/Types/HTypeCode.cs
@@ -0,0 +1,70 @@
+﻿#region  ******** Copyright © 2016 HauCore ********
+using System;
+#endregion  ******** Copyright © 2016 HauCore ********
+
+namespace HauCore.Types
+{
+    /// <summary>
+    /// Type code định nghĩa
+    /// </summary>
+    [Flags]
+    public enum HTypeCode
+    {
+        [TypeCodeOf(typeof(int))]
+        Int32,
+
+        [TypeCodeOf(typeof(long))]
+        Int64,
+
+        [TypeCodeOf(typeof(string))]
+        String,
+
+        [TypeCodeOf(typeof(decimal))]
+        Decimal,
+
+        [TypeCodeOf(typeof(bool))]
+        Boolean,
+
+        [TypeCodeOf(typeof(DateTime))]
+        DateTime,
+
+        [TypeCodeOf(typeof(TimeSpan))]
+        TimeSpan,
+
+        [TypeCodeOf(typeof(byte))]
+        Byte,
+
+        [TypeCodeOf(typeof(double))]
+        Double,
+
+        [TypeCodeOf(typeof(Guid))]
+        Guid,
+
+        [TypeCodeOf(typeof(short))]
+        Int16,
+
+        [TypeCodeOf(typeof(float))]
+        Single,
+
+        [TypeCodeOf(typeof(DBNull))]
+        DBNull,
+
+        UnKnown
+    }
+    /// <summary>
+    /// Mở rộng phương thức cho TypeCode
+    /// </summary>
+    public static class HTypeCodeExtender
+    {
+        /// <summary>
+        /// Kiểm tra xem Type Code có hợp lệ, có nằm trong list Type Code cần check
+        /// </summary>
+        /// <param name="typeCode"></param>
+        /// <param name="typeCodeChecking"></param>
+        /// <returns></returns>
+        public static bool IsSet(this HTypeCode typeCode, HTypeCode typeCodeChecking)
+        {
+            return (typeCode & typeCodeChecking) == typeCodeChecking;
+        }
+    }
+}
---
+++ HauCore/HauCore/Types/TypeCodeOfAttribute.cs
@@ -0,0 +1,24 @@
+﻿#region  ******** Copyright © 2016 HauCore ********
+using HauCore.Attributes;
+using System;
+#endregion  ******** Copyright © 2016 HauCore ********
+
+namespace HauCore.Types
+{
+    public class TypeCodeOfAttribute:FieldInfoAttribute
+    {
+        private Type type = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Type Type
+        {
+            get { return type; }
+        }
+        public TypeCodeOfAttribute(Type type)
+        {
+            this.type = type;
+        }
+
+    }
+}
