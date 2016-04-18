cfc8df552cc5f0ad347b21d172f2f3871cbba7e6
Nguyễn Văn Hậu <nguonsang@hotmail.com.vn>
2016-04-18 15:04:11 +0700

    Dot net 4.0

--- HauCore/HauCore/Properties/AssemblyInfo.cs
+++ HauCore/HauCore/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HauCore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("HauCore")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HauCore")]
 [assembly: AssemblyProduct("HauCore")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
